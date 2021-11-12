4a31f13276f0b8c5adefc68637df7f3c3362baae
henriquesbezerra <henriquesb92@gmail.com>
2021-11-12 20:52:27 -0300

    :construction: Testing plugin image in ghp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 
 // Step 2: Define your component
@@ -9,8 +9,11 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <title>Home Page</title>
         <h1>Welcome to my Gatsby site!</h1>
-        <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/dozero.webp"
+        />
     </Layout>
   )
 }
